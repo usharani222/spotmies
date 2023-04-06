e71e8a41ce86c51ad5ded5ea283f7f13dec4c086
sekharjavvadi <sekharatece@gmail.com>
2023-04-06 13:03:26 +0530

    tailwind css integrated with next js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#142440",
+        secundary: "#4ac8ed",
+        bg2: "#e2e8f0",
+      },
+    },
+  },
+  plugins: [],
+};
